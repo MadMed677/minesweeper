f054c9ef1ca0d6c9fab6408506ec18f9df65fd09
Artem Anashev <madmed677@gmail.com>
2022-01-25 19:21:36 +0500

    Add information how to deal with String in structs for WASM

--- src/wasm/wasm_types.rs
+++ src/wasm/wasm_types.rs
@@ -16,6 +16,20 @@ pub struct WasmCType {
     pub value: u8,
 }
 
+/// The ordinary `Cell` but only for Web Assembly
+///
+/// Current implementation uses id as an integer. But if we
+///  would like to make them String we have to:
+///  - Implement all fields as `getter` and in the struct
+///     we are not published the fields with `pub` keyword
+///
+/// We have to do this because wasm have to derive `copy` and `clone` traits
+///  when we expose the public fields. Because `String` [couldn't be copy](https://doc.rust-lang.org/std/marker/trait.Copy.html)
+///  we have to explicitly say that the structure themselves has no public fields
+///  and provide via `getters` all these fields
+///
+/// - [Why we couldn't use `String` as public in structures for Wasm](https://github.com/rustwasm/wasm-bindgen/issues/1775#issuecomment-533761425)
+/// - [Getters and Setters](https://rustwasm.github.io/docs/wasm-bindgen/reference/attributes/on-rust-exports/getter-and-setter.html)
 #[wasm_bindgen]
 pub struct WasmCell {
     pub id: CellId,
